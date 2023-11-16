5b8bc97aa6b5846c3e0217ad1351d40e1a6843c4
333divy <divyanandyadav@rocketmail.com>
2023-11-16 16:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const fs = require('fs');
 
 const app = express();
 app.set('view engine', 'ejs');
